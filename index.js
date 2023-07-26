9f3a653ab2185521c41882d2750fc98b4b59c1ff
faguirre1304 <f.aguirre1304@gmail.com>
2023-07-25 20:08:15 -0500

    agregando javascript a las secciones

--- index.js
+++ index.js
@@ -13,3 +13,38 @@ dots.forEach((cadaPunto, i) => {
     dots[i].classList.add("dotActivo");
   });
 });
+
+// tarjetas section try
+
+var alturaPantalla = window.innerHeight / 2;
+var card = document.querySelector(".try__grid");
+
+window.addEventListener("scroll", function () {
+  var alturaTarjeta = card.getBoundingClientRect().top;
+
+  if (alturaTarjeta >= alturaPantalla) {
+    card.classList.remove("aparecer__tarjeta");
+  } else {
+    card.classList.add("aparecer__tarjeta");
+  }
+});
+
+//tarjetas enjoy
+
+window.addEventListener("scroll", function () {
+  let alturaPantalla = window.innerHeight / 3;
+  let enjoyText = document.querySelector(".enjoy__text");
+  let enjoyCards = document.querySelector(".enjoy__cards");
+  var alturaTarjeta = enjoyText.getBoundingClientRect().top;
+
+  if (alturaTarjeta >= alturaPantalla) {
+    enjoyText.style.opacity = 0;
+    enjoyCards.style.opacity = 0;
+  } else {
+    enjoyText.style.opacity = 1;
+
+    setTimeout(function () {
+      enjoyCards.style.opacity = 1;
+    }, 500);
+  }
+});
